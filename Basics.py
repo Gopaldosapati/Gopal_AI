29c4325eaedd9cb88558817deadde096a073b529
gopal <dpgopal@gmail.com>
2026-07-08 08:35:50 +0530

    new file

---
+++ Basics.py
@@ -0,0 +1,9 @@
+from rich import print
+print("[red bold] Hello [/red bold]")
+# # print("Hello this is first line",end=",")
+# # print(100+300)
+# # name=input('enter your name:')
+# # print(name)
+# x=10
+# y=20
+# print(x+y)
---
+++ variables.py
@@ -0,0 +1,56 @@
+#string is a colletion of characters
+# can create a string with double quotes,single quote and tripple quotes
+#in creating paragraphs we can use tripple quotes
+
+# wish="welcome"
+# print(wish)
+
+# name="VPro"
+# print("welcome to ",name)
+
+#adding 2 strings****************************
+# name="VPro"
+# msg="welcome " + name
+# print(msg)
+
+## paragraph message
+
+# msg=""" hello
+#         how are you
+#         Gopal"""
+# print(msg)
+
+#using format (f) calling multiple varibles in the print
+
+# course="Agentic AI"
+# version=1.0
+# print(f"welcome to {course} and version is {version}")
+# name="Rahul"
+# age=24
+# print("Student name is {} and his age is {}".format(name,age))
+
+#different styles in printing a statement*****************
+
+# print("GenAI\n AgenticAI")
+# print("GenAI\t AgenticAI")
+# print('GenAI "AgenticAI"')
+
+# print("LLM ","GEN AI",sep="$")
+# print("LLM ","GENAI",sep='$')
+
+# to check type of a variable
+# name="VPro"
+# print(type(name))
+
+
+#INTEGERS
+
+num1=200
+num2=300
+add=num1+num2
+sub=num1-num2
+div=num1/num2
+print(num1+num2)
+print("addition of numbers:",add)
+print(f"subtraction of number is {sub}")
+print("division of numbers{}",format(div))
