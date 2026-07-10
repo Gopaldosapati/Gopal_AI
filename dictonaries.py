6a9c87a02c28b39dae17e27fc4fbadb08cd3bf60
gopal <dpgopal@gmail.com>
2026-07-10 08:34:58 +0530

    new lessons

---
+++ dictonaries.py
@@ -0,0 +1,19 @@
+# Keuys are immutable and values are mutable
+# represented in {}
+
+# d1={
+#     "name":"vpro",
+#     "sub":"AgenticAI",
+#     "Version":1.0
+# }
+# print(d1)
+# print(d1.keys())
+# print(d1.values())
+# print(d1.items())
+# d1={}
+# print(type(d1))
+
+d2={
+    (10,20):"Hello"
+}
+print(d2)
---
+++ sets.py
@@ -0,0 +1,18 @@
+# # set never allows duplicates ,unordered, hetergeneous and no indexes,searchable. 
+# # Hashable.represented as set() or {}
+
+# s1={10,20,30,10,20,30}
+# print(s1)-- removed duplicates
+
+# s1=set((10,20,30,10))
+# print(s1)
+
+# s1=set([10,20,10,30])
+# print(s1)
+
+# s5=set()
+# s6={}
+# print(type(s5))---its a dictionary empty
+# print(type(s6))---its a empty set
+
+
