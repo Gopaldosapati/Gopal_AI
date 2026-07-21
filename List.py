287bb4f64ec548ba1a930cac7f7845557d07e20a
gopal <dpgopal@gmail.com>
2026-07-21 07:32:23 +0530

    new concepts

--- List.py
+++ List.py
@@ -89,18 +89,73 @@
 #ex deepcopy
 # import copy
 # a=[[10,20],[30,40,50]]
-# b=copy.deepcopy(a)
+# b=copy.deepcopy(a) # copy the elements of a. If a modify b wont.
 # a[0][0]=100
 # print(a)
 # print(b)   # elements wont change
 
+# #ex:
+# a=[1,2,3,4,5,6,7,1,1]
+# print(len(a))
+# print(max(a))
+# print(sum(a))
+# print(sum(a)/len(a))
+# print(a.count(1))
+# b=sorted(a)  # sorted elements will be added to b
+# print(b)
+# print(a)
+
+
+#Ex: removing duplicatees using fromkeys in a list
+# a=[1,2,2,3,3,4,5,6,6,7]
+# uniq_a=list(dict.fromkeys(a))
+# print(uniq_a)
+
+#ex: 2d and 3d list creation
+# a=[[]]
+# print(a*3)
+# b=a*3
+# print(b)
+
+#ex:get the memory loction of a index
+# a=[10,20,30]
+# print(id(a))
+# a.append(40)   #append will not change the memory location
+# print(id(a))
+
 #ex:
-a=[1,2,3,4,5,6,7,1,1]
-print(len(a))
-print(max(a))
-print(sum(a))
-print(sum(a)/len(a))
-print(a.count(1))
-b=sorted(a)  # sorted elements will be added to b
-print(b)
-print(a)
+# a=[10,20,30]
+# print(id(a))
+# b=a+[40]   # will change the memory location
+# print(id(b))
+
+#ex: get the even cube results and sum the result using map,reduce and filter finctions
+# from functools import reduce
+# a=[1,2,3,4,5]
+# cubes=map(lambda x:x*x*x,a) #
+# even=filter(lambda b:b%2==0,cubes)
+# res=reduce(lambda num1,num2:num1+num2,even)
+# print(res)
+
+#ex: remove false values
+# a=[0,1,False,3,4]
+# res=list(filter(None,a))  #None is used to remove false values like 0,False,''
+# print(res)
+
+# a=[0,1,False,3,4]
+# res=list(filter(None,a))  #None is used to remove false values like 0,False,''
+# print(res)
+
+#ex:
+# a=[1,2,3,4,5]
+# print([i for i in a if i%2==0])
+
+#ex:
+# a=[1,2,3]
+# a.append([4,5])
+# print(a)
+
+# a=[1,2,3]
+# a.extend([4,5]) # extend used to add 4,5 to the list a
+# print(a)
+
--- Tuple.py
+++ Tuple.py
@@ -1,10 +1,59 @@
-#Tuple is a collection of values,heterogeneous ,immutable,ordered,index starts with 0
+#Tuple is a collection of values,heterogeneous ,immutable,ordered,index starts with 0 
+# and supports negative indexes
 # represent with ()
 
-a=(10,20,30,40,50)
-print(a)
-print(type(a))
-print(a[1:4])
+# a=(10,20,30,40,50)
+# print(a)
+# print(type(a))
+# print(a[1:4])
 
-a[2]=100
-print(a)
+# a[2]=100
+# print(a)
+
+#                    2026-07-20
+
+# a=100,4
+# print(type(a))
+
+# a=100,20,30
+# print(type(a))
+# b=tuple([10,20,30])
+# print(type(b))
+
+#Ex: 
+# t=(10,20,30)
+# list=list(t)  # converting tuple to list
+# list[0]=100  # modifying the list values
+# t2=tuple(list) # converting list to tuple
+# print(t2)
+# print(t)  #original tuple not modified
+
+#Ex:
+# a=(1,2,3,1,3,2,4,5)
+# print(a.count(1))
+# print(a.index(3))  #gives first occurance of the element index
+
+#ex:
+# a=10,20,30
+# e1,e2,e3=a
+# print(e1,e2,e3)
+
+# t=10,20,30,40,50
+# e1,*e2,e3=t  #e2=[20,30,40]
+# a1,*a2=e2    #a2=[30,40]
+# x,y=a2
+# print(e1,a1,x,y)
+
+#ex: function returning the tuple
+# def test():
+#     return 10,20,30
+# t1=test()
+# e1,e2,e3=t1
+# print(e1,e2,e3)
+
+#ex:
+# t1=((10,20),(30,40),(50,60))
+# for inner_list in t1:
+#     for index,element in enumerate(inner_list):
+#         print(index,element,sep="----->")
+
