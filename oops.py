1ed79849d8de4c92d3a02beab4793e45b3e7d614
gopal <dpgopal@gmail.com>
2026-07-22 08:53:01 +0530

    opps concept

---
+++ oops.py
@@ -0,0 +1,79 @@
+# collection of variablees(properties) and functions(behaviour) called as class
+# "class" is a keyword used to declre the classes
+# "pass" is the key word used to declare empty class
+# sharing same copy to multiple objects called as "instance members"
+# "self" is the keyword used to declare instance member
+
+# 1)inheritance   2)polymorphism    3)encapsulation    4)abstraction
+
+#ex:
+# class test:
+#     pass
+# obj1=test()  #constructor
+# print(obj1)  #hexa decimal value return 
+# print(id(obj1))  # convert to decimal
+
+#ex
+# class Test:
+#     num1=100
+# obj1=Test()
+# print(obj1.num1)
+
+#ex:
+# class Test:
+#     num1=100
+#     num2=200
+# obj1=Test()
+# add=obj1.num1+obj1.num2
+# print(add)
+
+#ex:
+# class Test:
+#     def test1(self):   #instance function
+#         print("Hello")
+#     def test2(self):
+#         return "Hello"
+#     def test3(self,param1):
+#         print(param1)
+#     def test4(self,param4):
+#         return param4
+
+# obj1=Test()
+# obj1.test1()
+# res1=obj1.test2()
+# print(res1)
+
+# obj1.test3("Hello")
+
+# res2=obj1.test4("Gopal")
+# print(res2)
+
+
+#ex : initialize parameters during obj creation
+
+# class Test:
+#     def __init__(self,x,y):
+#         self.num1=x
+#         self.num2=y
+# obj1=Test(100,200)
+# add=obj1.num1+obj1.num2
+# print(add)
+
+
+#ex: 
+# class test:
+#     def __init__(self,num1):
+#         self.num1=num1
+
+#     def test1(self):
+#         print(self.num1)
+#     def test2(self):
+#         return self.num1
+
+# obj1=test(200)
+# obj1.test1()
+# res=obj1.test2()
+# print(res)
+
+#ex:
+
--- sets.py
+++ sets.py
@@ -33,4 +33,33 @@
 # s5=set({30,20,10})
 # print(s5)
 
+############################22-07-2026#####################
+#ex:
+# s1={10,20}
+# s1.add(30)
+# s1.update([40,50])   #if we add more elements we need to use update function
+# s1.remove(10)   #to delete a particular element
+# print(s1)  # unorderd
+# print(s1.pop())
+# print(s1)
+# s1.clear()  #to clear all the elements
+# print(s1)
+
+
+#ex: check the element present or not
+# s1={10,20,30}
+# print(20 in s1)
+# print(40 in s1)
+# print(200 in s1)
+
+#ex:
+# s1={1,2,3}
+# s2={3,4,5}
+# print(s1.union(s2))  #it will remove duplicate in both and give uniq
+# print(s1 | s2)       #it will remove duplicate in both and give uniq
+# print(s1.intersection(s2)) #it will common element in both sets
+# print(s1 & s2) #it will common element in both sets
+# print(s1 - s2)   # remove the common element and return other elements from s1
+# print(s1 ^ s2)   # remove common element fromboth sets and return other elements
+
 #ex:
