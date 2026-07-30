f8a51ecba37993d1ea4730f8c8ee5e7a19644e1a
gopal <dpgopal@gmail.com>
2026-07-30 08:44:29 +0530

    pandas

--- 1-Pandas.py
+++ 1-Pandas.py
@@ -59,3 +59,93 @@ res=pd.read_csv("employees.csv")
 
 #print(res[res["Department"]=="IT"]["Salary"].max())
 #print(res["Name"].str.upper())
+
+#--------------------2026-07-30----------------------------
+
+# import pandas as pd
+# df=pd.read_csv("employees_simple.csv")
+# print(df)
+# # print(df.to_string(index=False))  #removing default indexes
+# print(df.set_index("Name",inplace=True))  # acts a index
+#print(df.loc[0:1])  #returns first 2 records. 0 include and 1 also include
+# print(df.loc[0,"Salary"])  #row label and column definition
+# print(df.iloc[2,2])
+# print(df.iloc[0:2])  #iloc is used to return 0 include and 2 exclude
+#print(df.set_index("Name"))  #using set_index we can set any column as a index
+#print(df.loc[:,["Name","Salary"]])
+#print(df.loc[2:,["Name","Salary"]])   #index 2 row with Name and Salary columns
+#print(df.loc[0:1:,["Name","Salary"]])  #0,1 index with Name and Salary columns
+
+#Ex:
+import pandas as pd
+# employees={
+#     "empid":[101,102,103,104,105],
+#     "Dept":["IT","HR","Finance","Tech","Admin"],
+#     "Salary":[10000,20000,15000,18000,13000],
+#     "Exp":[2,5,4,6,3],
+#     "Name":["Sam","Jhon","Ellen","Mike","Don"]
+# }
+# df=pd.DataFrame(employees)
+# print(df)
+# print(df.sort_values("Salary",ascending=False))
+#print(df.sort_values(by=["Dept","Salary"],ascending=(False,False)))
+#print(df.sort_values("Salary",ascending=False).head(1))  #Highest salary
+#print(df.groupby("Dept")["Salary"].sum())
+#print(df.groupby("Dept")["Salary"].agg(["min","max","sum","mean","count()"]))
+
+#Ex:
+
+# import pandas as pd
+# emps = {
+
+#     "EmpID" : [101,102,103,104,105],
+
+#     "Name" : ["Sam","John","David","Priya","Anjali"]
+
+# }
+
+# salaries = {
+
+#     "EmpID" : [101,102,103,104,106],
+
+#     "Salary" : [55000,70000,45000,90000,60000]
+
+# }
+# df1=pd.DataFrame(emps)
+# df2=pd.DataFrame(salaries)
+#print(pd.merge(df1,df2,on="EmpID"))
+# print(pd.merge(df1,df2,on="EmpID",how="left"))
+# print(pd.merge(df1,df2,on="EmpID",how="right"))
+#print(pd.merge(df1,df2,on="EmpID",how="outer"))
+
+#Ex: to concate rows data we have to use concat function 
+#column wise concat by using axis=1
+
+# print(pd.concat([df1,df2]))
+# print(pd.concat([df1,df2]),axis=True)
+
+
+#ex: calculate annual salary
+# import pandas as pd
+# employees={
+#     "empid":[101,102,103,104,105],
+#     "Dept":["IT","HR","Finance","Tech","Admin"],
+#     "Salary":[10000,20000,15000,18000,13000],
+#     "Exp":[2,5,4,6,3],
+#     "Name":["Sam","Jhon","Ellen","Mike","Don"]
+# }
+# df=pd.DataFrame(employees)
+# # df["Annual_Salary"]=df["Salary"]*12  #addding new column
+# # print(df)
+
+# df.drop("Exp",axis=1,inplace=True)
+# print(df)
+
+#Ex:
+import pandas as pd
+df=pd.read_csv("employees_null.csv")
+#print(df)
+# print(df.isnull())
+#print(df.isnull().sum())
+#print(df.fillna({"Age": 0}))  # replace null with 0 in Age column
+print(df["Salary"].fillna(df["Salary"].mean(),inplace=True))
