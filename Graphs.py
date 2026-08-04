93068aa154a662430143462ab99ce4e0d5724dd8
gopal <dpgopal@gmail.com>
2026-08-04 08:04:58 +0530

    new topics

--- Graphs.py
+++ Graphs.py
@@ -29,7 +29,26 @@
 # plt.show()
 
 #ex:multiline 
-import matplotlib.pyplot as plt
-months=["jan","feb","mar","apr","may","jun"]
-sales=[100,200,300,400,500,600]
-profits=[10,20,12,15,17,18]
+# import matplotlib.pyplot as plt
+# months=["jan","feb","mar","apr","may","jun"]
+# sales=[100,200,300,400,500,600]
+# profits=[10,20,12,15,17,18]
+ 
+#Ex: Barcharts
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# df=pd.read_csv("monthly_sales.csv")
+# month=df["Month"]
+# sales=df["Sales"]
+# plt.figure(figsize=(10,6))
+# bars=plt.bar("month","sales",color="skyblue",width=8.6,label="sales")
+# for bar in bars:
+#     plt.text(bar.get_x()+bar.get_width()/2,bar.get_height()+3,ha="center",fontsize=10)
+
+# plt.title("month-sales",fontsize=10,fontweight="bold")
+
+
+# plt.legend()
+# plt.show()
+
+#Ex: Pychart
