af25ccf96f62642f1db02fe3d69e27a3c1dc489d
gopal <dpgopal@gmail.com>
2026-08-06 08:53:47 +0530

    moved to python folder

--- Basics.py
+++
@@ -1,9 +0,0 @@
-from rich import print
-print("[red bold] Hello [/red bold]")
-# # print("Hello this is first line",end=",")
-# # print(100+300)
-# # name=input('enter your name:')
-# # print(name)
-# x=10
-# y=20
-# print(x+y)
